42b0068adc34961968f034259d76c60fbef1b7ab
Mike <michukimyxx@gmail.com>
2014-11-01 16:16:09 +0200

    1.0 Creation of Controller

---
+++ GameCode/src/main/java/SimulationGenerator.java
@@ -0,0 +1,17 @@
+
+public class SimulationGenerator {
+//	operations:
+//	      Client +rndClient()
+//	      Set<Client> +rndPopulation(Integer populationSize)
+//	      Employee +rndEmployee(EmployeeType employeeType)
+//	      Set<Employee> +rndStaff(Map<EmployeeType, Integer> employeeCounts)
+//	  	Set<Integer> +rndCombination(Integer k, Integer n)
+//	      Set<T> +rndCombination(Integer k, Set<T> things)
+//	      String +rndPhoneNumber()
+//	      String +rndForeName()
+//	      String +rndSurname()
+//	      String +rndTaxCode()
+	
+	
+
+}
